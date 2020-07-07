c1fe60d3b88c6bb582ba3406107e6817c0710c83
AthKas <65503934+AthKas@users.noreply.github.com>
2020-07-07 19:11:10 +0300

    Update Instacart.py

--- Instacart.py
+++ Instacart.py
@@ -591,7 +591,7 @@ parameters = {"objective":'binary:logistic',
             "subsample":0.7,
             "lambda": 0.95,
             "min_child_weight": 0.7,
-            "eta": 0.075,
+            "eta": 0.076,
             "gamma": 6,
                "gpu_id":0,
                "tree_method": 'gpu_hist'
@@ -599,7 +599,7 @@ parameters = {"objective":'binary:logistic',
 ########################################
 ########################################
 model = xgb.train(dtrain= D_train, params=parameters , num_boost_round=500)
-xgb.plot_importance(model)
+#xgb.plot_importance(model)
 
 # Delete X_train , y_train
 del [X_train, y_train]
